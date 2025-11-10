6d14cad3a9cd4f605d821a06826aceb1730a9e02
ShakilaNimanthaDilshan <ict22063@std.uwu.ac.lk>
2025-11-10 11:30:17 +0530

    Initial project upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+            Console.WriteLine("...... Welcome to the Calculator.....");
+
+            Console.WriteLine("Enter your first number : ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number : ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
