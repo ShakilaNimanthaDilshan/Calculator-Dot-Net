cf2425133554dee60ce1e55c19715ea6a927a698
ShakilaNimanthaDilshan <ict22063@std.uwu.ac.lk>
2025-11-10 11:55:53 +0530

    second commit

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -12,6 +12,8 @@ namespace Calculator
         {
             int num1;
             int num2;
+            string operation;
+            int result;
 
             Console.WriteLine("...... Welcome to the Calculator.....");
 
@@ -20,6 +22,35 @@ namespace Calculator
 
             Console.WriteLine("Enter your second number : ");
             num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What type of operation you whould like to do : ");
+            Console.WriteLine("Enter 'a' for addition, 's' for substraction, 'd' for divition, 'm' for multiplication ");
+            operation = Console.ReadLine();
+
+            if (operation == "a")
+            {
+                result = num1 + num2;
+            }
+            else if (operation == "s")
+            {
+                result = num1 - num2;
+            }
+            else if (operation == "d")
+            {
+                result = num1 / num2;
+            }
+            else if (operation == "m")
+            {
+                result = num1 * num2;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect Input");
+                return;
+
+            }
+
+            Console.WriteLine("Answer is : " + result);
         }
     }
 }
